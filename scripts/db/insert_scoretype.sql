79a63c8e4e0206da725f2185071cb2d502cac43d
Steve Aschwanden <steve.aschwanden@bluewin.ch>
2017-08-01 12:16:32 +0200

    Add piotroski f-score to database

--- scripts/db/insert_scoretype.sql
+++ scripts/db/insert_scoretype.sql
@@ -1,2 +1,3 @@
 insert into tscoretype (score_type_id, name) values (1, 'Levermann');
 insert into tscoretype (score_type_id, name) values (2, 'Magic Formula');
+insert into tscoretype (score_type_id, name) values (3, 'Piotroski F-Score');
