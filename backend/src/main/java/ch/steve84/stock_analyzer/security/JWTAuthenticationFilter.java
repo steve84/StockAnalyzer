4eae2b539f672ffb48fbe066da472ed8a1181fe2
Steve Aschwanden <steve.aschwanden@bluewin.ch>
2017-09-20 18:22:13 +0200

    Add spring security and jwt

---
+++ backend/src/main/java/ch/steve84/stock_analyzer/security/JWTAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package ch.steve84.stock_analyzer.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+public class JWTAuthenticationFilter extends GenericFilterBean {
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+	    Authentication authentication = TokenAuthenticationService
+	    		.getAuthentication((HttpServletRequest)req);
+
+	        SecurityContextHolder.getContext()
+	            .setAuthentication(authentication);
+	        chain.doFilter(req, res);
+	}
+
+}
---
+++ backend/src/main/java/ch/steve84/stock_analyzer/security/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package ch.steve84.stock_analyzer.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// 
+		http.csrf().disable().authorizeRequests()
+			.antMatchers(HttpMethod.POST, "/login").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.addFilterBefore(new JWTLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+			.addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// Add default user
+		auth.inMemoryAuthentication()
+			.withUser("admin")
+			.password("admin")
+			.roles("ADMIN");
+	}
+
+}
