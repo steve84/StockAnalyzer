378252ae53aab37fbfa31956775521acdae07305
Steve Aschwanden <steve.aschwanden@bluewin.ch>
2017-11-14 16:15:56 +0100

    Check all neccessary keys before inserting stocks Add debug flag

--- scripts/loadQuandlStocks.py
+++ scripts/loadQuandlStocks.py
@@ -18,6 +18,8 @@ parser.add_argument('-p', dest='db_pwd', help='database password')
 parser.add_argument('-d', dest='db_name', help='database name')
 parser.add_argument('-k', dest='quandl_key', help='quandl api key')
 parser.add_argument('--host', dest='db_host', help='database name')
+parser.add_argument('--debug', dest='debug', action='store_true', help='print out debug information')
+parser.add_argument('--page', dest='page', help='starting page')
 
 maxItems = parser.parse_args().maxItems
 db_user = parser.parse_args().db_user
@@ -25,17 +27,24 @@ db_pwd = parser.parse_args().db_pwd
 db_name = parser.parse_args().db_name
 db_host = parser.parse_args().db_host
 quandl_key = parser.parse_args().quandl_key
+debug = parser.parse_args().debug
+starting_page = parser.parse_args().page
 
 conn = psycopg2.connect("dbname=%s user=%s host=%s password=%s" % (db_name, db_user, db_host, db_pwd))
 
 totalHits = -1
-page = 1
-totalPages = 1
+if starting_page is None:
+    page = 1
+else:
+    page = int(starting_page)
+totalPages = 100000
 pageSize = 100
 
 baseUrl = 'https://www.quandl.com/api/v3/datasets.json'
 databaseCode = 'RB1'
 priceDatabaseCode = 'SSE'
+
+mandatoryKeys = {'name', 'country', 'branch', 'branch_group', 'isin', 'currency'}
 
 # Program logic
 noneStr = lambda s: '' if s is None else str(s)
@@ -45,7 +54,6 @@ totalInserted = {'countries': 0, 'branches': 0, 'stocks': 0}
 
 existing_countries = dict()
 existing_branches = dict()
-
 while page <= totalPages:
     cur = conn.cursor()
     query_params = dict()
@@ -56,15 +64,17 @@ while page <= totalPages:
     query_params['frequency'] = 'daily'
     query_params['api_key'] = quandl_key
 
+    if debug:
+        print('Fetch page %s of %s' % (str(page), str(totalPages)))
+
     link = baseUrl + '?' +  urllib.parse.urlencode(query_params)
+    print(link)
     response = requests.get(link)
     if response.status_code == 200:
         json_response = response.json()
         if totalHits < 0:
             totalHits = json_response['meta']['total_count']
             totalPages = json_response['meta']['total_pages']
-        
-        page = json_response['meta']['next_page']
         
         stocks = json_response['datasets']
         
@@ -96,55 +106,60 @@ while page <= totalPages:
                 stock['country_id'] = None
                 stock['branch_id'] = None
                 stock['created_at'] = Utils.getActualDate()
-                    
-                # select
-                if not noneStr(stock['country']) in existing_countries.keys():
-                    cur.execute("""SELECT * FROM tcountry WHERE name = %(country)s;""", stock)
-                    country = cur.fetchone()
-                    if not country:
-                        # insert
-                        cur.execute("""INSERT INTO tcountry (name, created_at) VALUES (%(country)s, %(created_at)s) RETURNING country_id;""", stock)
-                        stock['country_id'] = cur.fetchone()[0]
-                        totalInserted['countries'] += 1
-                    else:
-                        stock['country_id'] = country[0]
-                    if stock['country']:
-                        existing_countries[noneStr(stock['country'])] = stock['country_id']
-                else:
-                    stock['country_id'] = existing_countries[noneStr(stock['country'])]
 
-                # select
-                if not (noneStr(stock['branch']) + noneStr(stock['branch_group']))  in existing_branches.keys():
-                    cur.execute("""SELECT * FROM tbranch WHERE name = %(branch)s;""", stock)
-                    branch = cur.fetchone()
-                    if not branch:
-                        # insert
-                        cur.execute("""INSERT INTO tbranch (name, branch_group, created_at) VALUES (%(branch)s, %(branch_group)s, %(created_at)s) RETURNING branch_id;""", stock)
-                        stock['branch_id'] = cur.fetchone()[0]
-                        totalInserted['branches'] += 1
+                if mandatoryKeys.issubset(set(stock.keys())):
+                    # select
+                    if not noneStr(stock['country']) in existing_countries.keys():
+                        cur.execute("""SELECT * FROM tcountry WHERE name = %(country)s;""", stock)
+                        country = cur.fetchone()
+                        if not country:
+                            # insert
+                            cur.execute("""INSERT INTO tcountry (name, created_at) VALUES (%(country)s, %(created_at)s) RETURNING country_id;""", stock)
+                            stock['country_id'] = cur.fetchone()[0]
+                            totalInserted['countries'] += 1
+                        else:
+                            stock['country_id'] = country[0]
+                        if stock['country']:
+                            existing_countries[noneStr(stock['country'])] = stock['country_id']
                     else:
-                        stock['branch_id'] = branch[0]
-                    if stock['branch']:
-                        existing_branches[noneStr(stock['branch']) + noneStr(stock['branch_group'])] = stock['branch_id']
-                else:
-                    stock['branch_id'] = existing_branches[noneStr(stock['branch']) + noneStr(stock['branch_group'])]
+                        stock['country_id'] = existing_countries[noneStr(stock['country'])]
 
-                # select
-                cur.execute("""SELECT * FROM tstock WHERE isin = %(isin)s;""", stock)
-                if cur.rowcount == 0:
-                    # insert
-                    cur.execute("""INSERT INTO tstock (name, isin, branch_id, country_id, currency, quandl_rb1_id, quandl_price_dataset, created_at) VALUES (%(name)s, %(isin)s, %(branch_id)s, %(country_id)s, %(currency)s, %(quandl_rb1_id)s, %(quandl_price_dataset)s, %(created_at)s);""", stock)
-                    totalInserted['stocks'] += 1
-                    totalProcessed += 1
+                    # select
+                    if not (noneStr(stock['branch']) + noneStr(stock['branch_group']))  in existing_branches.keys():
+                        cur.execute("""SELECT * FROM tbranch WHERE name = %(branch)s;""", stock)
+                        branch = cur.fetchone()
+                        if not branch:
+                            # insert
+                            cur.execute("""INSERT INTO tbranch (name, branch_group, created_at) VALUES (%(branch)s, %(branch_group)s, %(created_at)s) RETURNING branch_id;""", stock)
+                            stock['branch_id'] = cur.fetchone()[0]
+                            totalInserted['branches'] += 1
+                        else:
+                            stock['branch_id'] = branch[0]
+                        if stock['branch']:
+                            existing_branches[noneStr(stock['branch']) + noneStr(stock['branch_group'])] = stock['branch_id']
+                    else:
+                        stock['branch_id'] = existing_branches[noneStr(stock['branch']) + noneStr(stock['branch_group'])]
+
+                    # select
+                    cur.execute("""SELECT * FROM tstock WHERE isin = %(isin)s;""", stock)
+                    if cur.rowcount == 0:
+                        # insert
+                        cur.execute("""INSERT INTO tstock (name, isin, branch_id, country_id, currency, quandl_rb1_id, quandl_price_dataset, created_at) VALUES (%(name)s, %(isin)s, %(branch_id)s, %(country_id)s, %(currency)s, %(quandl_rb1_id)s, %(quandl_price_dataset)s, %(created_at)s);""", stock)
+                        totalInserted['stocks'] += 1
+                        totalProcessed += 1
+                else:
+                    if debug:
+                        print('Cannot load stock %s (%s)' % (stock['name'], stock['quandl_rb1_id']))
 
             if maxItems and totalProcessed >= maxItems:
                 break
         conn.commit()
         if maxItems and totalProcessed >= maxItems:
             break
-    
     else:
-        break
+        print('Status code %s' % response.status_code)
+    page += 1
+
 
     
 
