df0cf051e9ec385022c71b3cbb2f86527e3cb837
Steve Aschwanden <steve.aschwanden@bluewin.ch>
2017-12-08 14:05:17 +0100

    Add language switch feature

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://agile-retreat-72375.herokuapp.com'
+  apiUrl: 'https://agile-retreat-72375.herokuapp.com',
+  baseUrl: 'https://immense-ocean-17042.herokuapp.com'
 };
