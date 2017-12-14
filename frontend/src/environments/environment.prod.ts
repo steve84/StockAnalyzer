47901fc57a5c97540f43e9b62fef2d72a815306f
Steve Aschwanden <steve.aschwanden@bluewin.ch>
2017-12-14 18:34:40 +0100

    Httpd rewrite rules

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://agile-retreat-72375.herokuapp.com',
-  baseUrl: 'https://immense-ocean-17042.herokuapp.com'
+  apiUrl: 'https://agile-retreat-72375.herokuapp.com'
 };
